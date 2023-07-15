b2a77a0c16ca6e9e15a5c1d495b729b427c97076
Dar <deadunlimited@gmail.com>
2023-07-15 13:23:43 +0300

    сделана задача 2 урока 1

---
+++ src/main/kotlin/lesson_1/task2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val countOrders = 0
+    val purchaseText = "Спасибо за приобретенный товар"
+    var countEmployees = 2000
+
+    println(countOrders)
+    println(purchaseText)
+
+    countEmployees--
+
+    println(countEmployees)
+}
