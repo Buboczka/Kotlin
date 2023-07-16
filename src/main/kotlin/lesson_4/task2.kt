6cd80f0f2c7ebfd14cb338a79610073117ffc06c
Dar <deadunlimited@gmail.com>
2023-07-16 13:30:05 +0300

    сделана задача 2 урока 4

---
+++ src/main/kotlin/lesson_4/task2.kt
@@ -0,0 +1,28 @@
+package lesson_4
+
+fun main() {
+    var weight = 42
+    var volume = 120
+
+
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':" +
+                " ${(weight >= 35 && weight <= 100) && volume < 100}"
+    )
+
+    weight = 20
+    volume = 80
+
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':" +
+                " ${(weight >= 35 && weight <= 100) && volume < 100}"
+    )
+
+    weight = 50
+    volume = 100
+
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':" +
+                " ${(weight >= 35 && weight <= 100) && volume < 100}"
+    )
+}
