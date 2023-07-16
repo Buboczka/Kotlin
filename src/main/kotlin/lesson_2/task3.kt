0287eba945922866a111cc0e46143f4b5dcbe611
Dar <deadunlimited@gmail.com>
2023-07-16 19:08:37 +0300

    fix name const

--- src/main/kotlin/lesson_2/task3.kt
+++ src/main/kotlin/lesson_2/task3.kt
@@ -1,17 +1,17 @@
 package lesson_2
 
-const val UNIT_TIME = 60
+const val MINUTES_IN_HOUR = 60
 
 fun main() {
     val departureHour = 9
     val departureMinute = 39
     val travelInMinutes = 457
 
-    val travelHour = travelInMinutes / UNIT_TIME
-    val remainingMinutes = travelInMinutes - (travelHour * UNIT_TIME)
+    val travelHour = travelInMinutes / MINUTES_IN_HOUR
+    val remainingMinutes = travelInMinutes - (travelHour * MINUTES_IN_HOUR)
     var arrivalMinute = departureMinute + remainingMinutes
-    val arrivalHour = departureHour + travelHour + arrivalMinute / UNIT_TIME
-    arrivalMinute -= ((arrivalMinute / UNIT_TIME) * UNIT_TIME)
+    val arrivalHour = departureHour + travelHour + arrivalMinute / MINUTES_IN_HOUR
+    arrivalMinute -= ((arrivalMinute / MINUTES_IN_HOUR) * MINUTES_IN_HOUR)
 
     println("${arrivalHour}:${arrivalMinute}")
 }
