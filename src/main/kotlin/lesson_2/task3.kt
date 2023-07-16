ead71e9ce434f73f2a591128114557ec964582d7
Dar <deadunlimited@gmail.com>
2023-07-16 19:05:07 +0300

    added const for time

--- src/main/kotlin/lesson_2/task3.kt
+++ src/main/kotlin/lesson_2/task3.kt
@@ -1,15 +1,17 @@
 package lesson_2
 
+const val UNIT_TIME = 60
+
 fun main() {
     val departureHour = 9
     val departureMinute = 39
     val travelInMinutes = 457
 
-    val travelHour = travelInMinutes / 60
-    val remainingMinutes = travelInMinutes - (travelHour * 60)
+    val travelHour = travelInMinutes / UNIT_TIME
+    val remainingMinutes = travelInMinutes - (travelHour * UNIT_TIME)
     var arrivalMinute = departureMinute + remainingMinutes
-    val arrivalHour = departureHour + travelHour + arrivalMinute / 60
-    arrivalMinute -= ((arrivalMinute / 60) * 60)
+    val arrivalHour = departureHour + travelHour + arrivalMinute / UNIT_TIME
+    arrivalMinute -= ((arrivalMinute / UNIT_TIME) * UNIT_TIME)
 
     println("${arrivalHour}:${arrivalMinute}")
 }
